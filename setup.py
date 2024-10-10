7ca9507d4e5cd775c05da7d9f793e59de1e61537
Marsmaxmax <winterrmaxl@gmail.com>
2024-10-10 22:05:41 +0200

    finished model

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from tensorflow.keras.models import Model
-from tensorflow.keras.layers import Dense, LSTM, Input, Concatenate, Dropout
+from tensorflow.keras.layers import Dense, LSTM, Input, Concatenate, Dropout, GlobalAveragePooling1D
 from util.datasequencer import create_sequences
 import os
 
@@ -22,19 +22,140 @@ x_close, x_open, x_high, x_low, x_rsi, x_ema20, x_ema50, x_ema100, y_up, y_down 
 #******Modell******
 #******************
 
-input_close = Input(shape=(100,1))
-input_open = Input(shape=(100,1))
-input_high = Input(shape=(100,1))
-input_low = Input(shape=(100,1))
-input_rsi = Input(shape=(100,1))
-input_ema20 = Input(shape=(100,1))
-input_ema50 = Input(shape=(100,1))
-input_ema100 = Input(shape=(100,1))
+input_close = Input(shape=(128,1))
+input_open = Input(shape=(128,1))
+input_high = Input(shape=(128,1))
+input_low = Input(shape=(128,1))
+input_rsi = Input(shape=(128,1))
+input_ema20 = Input(shape=(128,1))
+input_ema50 = Input(shape=(128,1))
+input_ema100 = Input(shape=(128,1))
 
-x = LSTM(64, return_sequences=False)(input_close)
+branch_0 = LSTM(128, return_sequences=True) (input_close)
+branch_0 = LSTM(128, return_sequences=True) (branch_0)
+branch_0 = Dense(128, activation = 'silu') (branch_0)
+branch_0 = LSTM(128, return_sequences=True) (branch_0)
+branch_0 = LSTM(128, return_sequences=True) (branch_0)
 
-output_up = Dense(1,name='up_trend')(x)
-output_down = Dense(1,name='down_trend')(x)
+branch_1 = LSTM(128, return_sequences=True) (input_open)
+branch_1 = LSTM(128, return_sequences=True) (branch_1)
+branch_1 = Dense(128, activation = 'silu') (branch_1)
+branch_1 = LSTM(128, return_sequences=True) (branch_1)
+branch_1 = LSTM(128, return_sequences=True) (branch_1)
+
+branch_2 = LSTM(128, return_sequences=True) (input_high)
+branch_2 = LSTM(128, return_sequences=True) (branch_2)
+branch_2 = Dense(128, activation = 'silu') (branch_2)
+branch_2 = LSTM(128, return_sequences=True) (branch_2)
+branch_2 = LSTM(128, return_sequences=True) (branch_2)
+
+branch_3 = LSTM(128, return_sequences=True) (input_low)
+branch_3 = LSTM(128, return_sequences=True) (branch_3)
+branch_3 = Dense(128, activation = 'silu') (branch_3)
+branch_3 = LSTM(128, return_sequences=True) (branch_3)
+branch_3 = LSTM(128, return_sequences=True) (branch_3)
+
+branch_4 = LSTM(128, return_sequences=True) (input_rsi)
+branch_4 = LSTM(128, return_sequences=True) (branch_4)
+branch_4 = Dense(128, activation = 'silu') (branch_4)
+branch_4 = LSTM(128, return_sequences=True) (branch_4)
+branch_4 = LSTM(128, return_sequences=True) (branch_4)
+
+branch_5 = LSTM(128, return_sequences=True) (input_ema20)
+branch_5 = LSTM(128, return_sequences=True) (branch_5)
+branch_5 = Dense(128, activation = 'silu') (branch_5)
+branch_5 = LSTM(128, return_sequences=True) (branch_5)
+branch_5 = LSTM(128, return_sequences=True) (branch_5)
+
+branch_6 = LSTM(128, return_sequences=True) (input_ema50)
+branch_6 = LSTM(128, return_sequences=True) (branch_6)
+branch_6 = Dense(128, activation = 'silu') (branch_6)
+branch_6 = LSTM(128, return_sequences=True) (branch_6)
+branch_6 = LSTM(128, return_sequences=True) (branch_6)
+
+branch_7 = LSTM(128, return_sequences=True) (input_ema100)
+branch_7 = LSTM(128, return_sequences=True) (branch_7)
+branch_7 = Dense(128, activation = 'silu') (branch_7)
+branch_7 = LSTM(128, return_sequences=True) (branch_7)
+branch_7 = LSTM(128, return_sequences=True) (branch_7)
+
+alltensors = Concatenate() ([branch_0,branch_1,branch_2,branch_3,branch_4,branch_5,branch_6,branch_7])
+datatensors = Concatenate() ([branch_0,branch_1,branch_2,branch_3])
+inditensors = Concatenate() ([branch_4,branch_5,branch_6,branch_7])
+
+alltensors = LSTM(128, return_sequences=True) (alltensors)
+alltensors = LSTM(128, return_sequences=True) (alltensors)
+alltensors = Dense(128, activation = 'relu6') (alltensors)
+alltensors = LSTM(128, return_sequences=True) (alltensors)
+alltensors = LSTM(128, return_sequences=True) (alltensors)
+
+branch_8  = LSTM(128, return_sequences=True) (datatensors)
+branch_8  = LSTM(128, return_sequences=True) (branch_8)
+branch_8 = Dense(128, activation = 'silu') (branch_8)
+branch_8  = LSTM(128, return_sequences=True) (branch_8)
+branch_8 = Concatenate() ([alltensors, branch_8])
+branch_8  = LSTM(128, return_sequences=True) (branch_8)
+branch_8 = Dense(128, activation = 'silu') (branch_8)
+branch_8  = LSTM(128, return_sequences=True) (branch_8)
+branch_8  = LSTM(128, return_sequences=True) (branch_8)
+
+branch_9  = LSTM(128, return_sequences=True) (datatensors)
+branch_9  = LSTM(128, return_sequences=True) (branch_9)
+branch_9 = Dense(128, activation = 'silu') (branch_9)
+branch_9  = LSTM(128, return_sequences=True) (branch_9)
+branch_9 = Concatenate() ([alltensors, branch_9])
+branch_9  = LSTM(128, return_sequences=True) (branch_9)
+branch_9 = Dense(128, activation = 'silu') (branch_9)
+branch_9  = LSTM(128, return_sequences=True) (branch_9)
+branch_9  = LSTM(128, return_sequences=True) (branch_9)
+
+branch_10  = LSTM(128, return_sequences=True) (inditensors)
+branch_10  = LSTM(128, return_sequences=True) (branch_10)
+branch_10 = Dense(128, activation = 'silu') (branch_10)
+branch_10  = LSTM(128, return_sequences=True) (branch_10)
+branch_10 = Concatenate() ([alltensors, branch_10])
+branch_10  = LSTM(128, return_sequences=True) (branch_10)
+branch_10 = Dense(128, activation = 'silu') (branch_10)
+branch_10  = LSTM(128, return_sequences=True) (branch_10)
+branch_10  = LSTM(128, return_sequences=True) (branch_10)
+
+branch_11  = LSTM(128, return_sequences=True) (inditensors)
+branch_11  = LSTM(128, return_sequences=True) (branch_11)
+branch_11 = Dense(128, activation = 'silu') (branch_11)
+branch_11  = LSTM(128, return_sequences=True) (branch_11)
+branch_11 = Concatenate() ([alltensors, branch_11])
+branch_11  = LSTM(128, return_sequences=True) (branch_11)
+branch_11 = Dense(128, activation = 'silu') (branch_11)
+branch_11  = LSTM(128, return_sequences=True) (branch_11)
+branch_11  = LSTM(128, return_sequences=True) (branch_11)
+
+branch_12 = Concatenate() ([branch_8, branch_10])
+branch_12  = LSTM(128, return_sequences=True) (branch_12)
+branch_12  = LSTM(128, return_sequences=True) (branch_12)
+branch_12 = Dense(128, activation = 'silu') (branch_12)
+branch_12  = LSTM(128, return_sequences=True) (branch_12)
+branch_12  = LSTM(128, return_sequences=True) (branch_12)
+branch_12 = Concatenate() ([alltensors, branch_12])
+branch_12  = LSTM(128, return_sequences=True) (branch_12)
+branch_12  = LSTM(128, return_sequences=True) (branch_12)
+
+branch_13 = Concatenate() ([branch_9, branch_11])
+branch_13  = LSTM(128, return_sequences=True) (branch_13)
+branch_13  = LSTM(128, return_sequences=True) (branch_13)
+branch_13 = Dense(128, activation = 'silu') (branch_13)
+branch_13  = LSTM(128, return_sequences=True) (branch_13)
+branch_13  = LSTM(128, return_sequences=True) (branch_13)
+branch_13 = Concatenate() ([alltensors, branch_13])
+branch_13  = LSTM(128, return_sequences=True) (branch_13)
+branch_13  = LSTM(128, return_sequences=True) (branch_13)
+
+output_up = GlobalAveragePooling1D()(branch_12)
+output_up = Dense(128)(output_up)
+output_up = Dense(1,activation = 'linear',name='up_trend')(output_up)
+
+output_down = GlobalAveragePooling1D()(branch_13)
+output_down = Dense(128) (output_down)
+output_down = Dense(1,activation = 'linear',name='down_trend')(output_down)
 
 # Modell mit mehreren Ausgaben erstellen
 model = Model(inputs=[input_close, input_open, input_high, input_low, input_rsi, input_ema20, input_ema50,input_ema100], outputs=[output_up, output_down])
---
+++ train.py
@@ -0,0 +1,47 @@
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from tensorflow.keras.models import Model, save_model, load_model
+from tensorflow.keras.layers import Dense, LSTM, Input, Concatenate, Dropout, GlobalAveragePooling1D
+from util.datasequencer import create_sequences
+import os
+import sys
+
+# sys.argv[0] ist der Name des Skripts
+# sys.argv[1] ist das erste Argument von der Kommandozeile
+if len(sys.argv) > 1:
+    print(f" Anzahl Durchläufe{sys.argv[1]}")
+else:
+    print("Keine Argumente eingegeben.")
+    exit()
+
+input_file = 'data/train1.csv'  # Name der Eingabedatei
+model_file = 'trend_model.keras'  # Name der Datei, in der das Modell gespeichert wird
+runs = int(sys.argv[1])
+
+data = pd.read_csv(input_file, header=None)
+
+candles = data.values  # Close, Open, High, Low
+
+# Daten in Sequenzen umwandeln
+x_close, x_open, x_high, x_low, x_rsi, x_ema20, x_ema50, x_ema100, y_up, y_down = create_sequences(candles)
+
+# Überprüfen, ob das Modell existiert und geladen werden kann
+if os.path.exists(model_file):
+    # Modell laden
+    model = load_model(model_file)
+    print(f'Modell "{model_file}" erfolgreich geladen.')
+else:
+    print(f'Modell nicht"{model_file}" gefunden')
+    exit()
+
+model.summary()
+
+y_combined = {'up_trend': y_up, 'down_trend': y_down}
+
+history = model.fit([x_close, x_open, x_high, x_low, x_rsi, x_ema20, x_ema50, x_ema100], y_combined, epochs=runs, batch_size=32, validation_split=0.2)
+
+# Modell speichern
+model.save(model_file)
+print(f'Modell wurde als "{model_file}" gespeichert.')
